5478f7d7ef70f82a07e50876f0061b352632dcbe
Hamza Mostafa <hamza.oruba@gmail.com>
2024-03-12 22:47:35 -0400

    Time: 3827 ms (70.40%) | Memory: 16.5 MB (95.86%) - LeetSync

--- 79-word-search/word-search.py
+++ 79-word-search/word-search.py
@@ -2,29 +2,36 @@ class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         ROWS, COLS = len(board), len(board[0])
 
+        directions = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+
         def dfs(row, col, index):
-            if index >= len(word):
+            # we found a match
+            if index == len(word):
                 return True
             
-            if(row not in range(ROWS) or col not in range(COLS) or board[row][col] == '.' or board[row][col] != word[index]):
+            # out of bounds, already visited, or wrong character
+            if row < 0 or row >= ROWS or col < 0 or col >= COLS or board[row][col] == "." or board[row][col] != word[index]:
                 return False
 
-            currChar = board[row][col]
-            # the dot marks the current cell as visited
-            board[row][col] = '.'
-
-            # dfs on next cell
-            if (dfs(row + 1, col, index + 1) or dfs(row - 1, col, index + 1) or dfs(row, col + 1, index + 1) or dfs(row, col - 1, index + 1)):
-                return True
-
-            # mark the cell as unvisited
-            board[row][col] = currChar
-            return False
+            curChar = board[row][col]
             
+            # mark as visited
+            board[row][col] = "."
 
-        # call dfs on each cell in the board
-        for row in range(ROWS):
-            for col in range(COLS):
-                if dfs(row, col, 0):
+            # explore neighbouring cells
+            for x, y in directions:
+                if dfs(row + x, col + y, index + 1):
                     return True
+
+            # backtrack
+            board[row][col] = curChar
+
+            return False
+
+        # call dfs starting from each cell
+        for i in range(ROWS):
+            for j in range(COLS):
+                if dfs(i, j, 0):
+                    return True
+
         return False
