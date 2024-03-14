ef9451eb081475c4aca356fc039be8e8b98d02b5
Hamza Mostafa <hamza.oruba@gmail.com>
2024-03-13 22:38:07 -0400

    Time: 467 ms (58.66%) | Memory: 35.4 MB (24.65%) - LeetSync

---
+++ 131-palindrome-partitioning/palindrome-partitioning.py
@@ -0,0 +1,38 @@
+class Solution:
+    def isPalindrome(self, s):
+        left, right = 0, len(s) - 1
+
+        while left < right:
+            if s[left] != s[right]:
+                return False
+
+            left += 1
+            right -= 1
+
+        return True
+
+    def partition(self, s: str) -> List[List[str]]:
+        res = []
+        currPartition = []
+
+        def dfs(i):
+            # we have reached the end of the current string
+            if i == len(s):
+                # append a copy of the current partition to the result list
+                res.append(currPartition[:])
+                return
+
+            for j in range(i, len(s)):
+                # current substring is a palindrome
+                if self.isPalindrome(s[i:j + 1]):
+                    # partition the string at index j + 1
+                    currPartition.append(s[i: j + 1])
+                    dfs(j + 1)
+
+                    # backtrack
+                    currPartition.pop()
+
+        dfs(0)
+
+        return res
+        
