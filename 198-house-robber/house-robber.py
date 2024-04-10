4770f9febcb4a2df1ff89b50954ceb33a0306972
Hamza Mostafa <hamza.oruba@gmail.com>
2024-04-10 19:00:14 -0400

    Time: 40 ms (25.88%) | Memory: 16.5 MB (89.68%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,10 +1,15 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        dp = [0] * (len(nums) + 1)
-        dp[-2] = nums[-1]
+        robCurrHouseAndOne = 0
+        doNotRobCurrHouse = nums[-1]
+
+        # need a variable for dp[i] (temp), dp[i + 1] (doNotRobCurrHouse), and dp[i + 2] (robCurrHouseAndOne)
+        # dp[i] = max(nums[i] + dp[i + 2], dp[i + 1])
 
         for i in reversed(range(len(nums) - 1)):
-            dp[i] = max(nums[i] + dp[i + 2], dp[i + 1])
+            temp = max(robCurrHouseAndOne + nums[i], doNotRobCurrHouse)
+            robCurrHouseAndOne = doNotRobCurrHouse
+            doNotRobCurrHouse = temp
 
-        return dp[0]
+        return doNotRobCurrHouse
         
