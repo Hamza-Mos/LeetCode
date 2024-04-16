f086189ecbb158fc5b454d09ef1e1d54693d9461
Hamza Mostafa <hamza.oruba@gmail.com>
2024-04-15 22:02:42 -0400

    Time: 132 ms (87.78%) | Memory: 22.9 MB (31.30%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.py
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.py
@@ -1,53 +1,27 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         ROWS, COLS = len(matrix), len(matrix[0])
-        # do binary search on diagonals (removes repeated work of visiting cells multiple times if 
-        # we did binary search on all possible rows and cols)
 
-        def binarySearch(start, vertical):
-            low = start
-            high = -1
-            # binary search on current col
-            if vertical:
-                high = ROWS - 1
+        row = ROWS - 1
+        col = 0
 
-            # binary search on current row
-            else:
-                high = COLS - 1
+        # keep traversing right until we hit a number that is greater than our target (move to larger numbers)
+        # then we keep traversing up until we hit a number less than our target (move to smaller numbers)
+        # repeat until we end loop or reach target
 
-            while low <= high:
-                mid = (low + high) // 2
-
-                col = row = -1
-
-                # binary search on row (col does not change)
-                if vertical:
-                    col = start
-                    row = mid
-
-                # same but for col
-                else:
-                    col = mid
-                    row = start
-
-                if matrix[row][col] == target:
-                    return True
-
-                elif matrix[row][col] < target:
-                    low = mid + 1
-
-                else:
-                    high = mid - 1
-
-
-        for i in range(min(ROWS, COLS)):
-            foundTargetInRow = binarySearch(i, False)
-            foundTargetInCol = binarySearch(i, True)
-
-            if foundTargetInRow or foundTargetInCol:
+        while row >= 0 and col < COLS:
+            # found target
+            if matrix[row][col] == target:
                 return True
 
-        return False
+            # current number is less than target so we move right to larger numbers
+            elif matrix[row][col] < target:
+                col += 1
 
+            # current number is greater than target so we move up to smaller numbers
+            else:
+                row -= 1
+
+        return False
             
         
