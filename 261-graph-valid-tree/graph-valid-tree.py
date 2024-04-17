7567e5498089e37decf9e390d488c63c7acc61e9
Hamza Mostafa <hamza.oruba@gmail.com>
2024-04-17 16:45:04 -0400

    Time: 87 ms (56.30%) | Memory: 18.5 MB (35.83%) - LeetSync

--- 261-graph-valid-tree/graph-valid-tree.py
+++ 261-graph-valid-tree/graph-valid-tree.py
@@ -3,17 +3,18 @@ class UnionFind:
         self.parent = [i for i in range(n)]
         self.rank = [1] * n
 
-    def find(self, n):
-        while self.parent[n] != n:
-            self.parent[n] = self.parent[self.parent[n]]
-            n = self.parent[n]
+    def find(self, node):
+        while self.parent[node] != node:
+            self.parent[node] = self.parent[self.parent[node]] # path compression
+            node = self.parent[node]
 
-        return n
+        return node
 
+    # True if redundant connection
+    # False otherwise
     def union(self, n1, n2):
         p1, p2 = self.find(n1), self.find(n2)
 
-        # already connected
         if p1 == p2:
             return True
 
@@ -23,25 +24,25 @@ class UnionFind:
 
         else:
             self.parent[p1] = p2
-            self.rank[p2] += self.rank[p1]
+            self.rank[p2] += p1
 
         return False
 
 class Solution:
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        # 2 criteria
+        # 1) no redundant connections
+        # 2) 1 connected component
+
         uf = UnionFind(n)
+        numComponents = n
 
-        # number of connected components (originally set to n because none of the nodes are connected)
-        res = n
-
-        for n1, n2 in edges:
-            # already connected (so invalid tree)
-            if uf.union(n1, n2):
+        for node1, node2 in edges:
+            if uf.union(node1, node2):
                 return False
 
-            # decrement number of connected components by 1
-            res -= 1
+            numComponents -= 1
 
-        # in a valid tree, there must only be 1 connected component
-        return res == 1
+        return numComponents == 1
+
         
