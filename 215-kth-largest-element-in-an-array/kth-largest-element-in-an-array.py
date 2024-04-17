44e71f0ff3243167b73aba2f57fdf7f2d2de4982
Hamza Mostafa <hamza.oruba@gmail.com>
2024-04-17 08:45:31 -0400

    Time: 464 ms (65.82%) | Memory: 30 MB (35.99%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
@@ -1,46 +1,17 @@
 class Solution:
-    def quickSelectSolution(self, nums, k):
-        kthIndex = len(nums) - k
-
-        def quickselect(start, end):
-            pivot = end
-            left = start
-
-            for i in range(start, end):
-                if nums[i] <= nums[pivot]:
-                    nums[left], nums[i] = nums[i], nums[left]
-                    left += 1
-
-            # swap value at left index with pivot
-            nums[left], nums[pivot] = nums[pivot], nums[left]
-
-            # left is now the new index of the pivot value
-
-            # need to do quick select on left portion of the array
-            if left > kthIndex:
-                return quickselect(start, left - 1)
-
-            # need to do quick select on right portion of the array
-            elif left < kthIndex:
-                return quickselect(left + 1, end)
-
-            # pivot marks the correct index
-            else:
-                return nums[left]
-
-        return quickselect(0, len(nums) - 1)
-
-    def heapSolution(self, nums, k):
-        minHeap = []
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        heap = []
 
         for n in nums:
-            heapq.heappush(minHeap, n)
+            if len(heap) < k:
+                heapq.heappush(heap, n)
+                continue
 
-            if len(minHeap) > k:
-                heapq.heappop(minHeap)
+            if len(heap) == k and n > heap[0]:
+                heapq.heappop(heap)
+                heapq.heappush(heap, n)
 
-        return minHeap[0]
+        return heap[0]
 
-    def findKthLargest(self, nums: List[int], k: int) -> int:
-        return self.heapSolution(nums, k)
+            
         
