cacc1efe9a9ea2db7081c0784f0c34517a640a6f
Hamza Mostafa <hamza.oruba@gmail.com>
2024-04-17 12:49:57 -0400

    Time: 37 ms (46.54%) | Memory: 16.8 MB (40.86%) - LeetSync

--- 355-design-twitter/design-twitter.py
+++ 355-design-twitter/design-twitter.py
@@ -1,58 +1,48 @@
 class Twitter:
 
     def __init__(self):
-        # maps a userId to a set of followeeIds
-        self.followMap = collections.defaultdict(set)
-
-        # maps a userId to a list of tweetIds
-        self.tweetMap = collections.defaultdict(list)
-
-        # acts as a timestamp of each tweet
-        self.count = 0
+        self.time = 0 # keeps track of post times for tweets
+        self.userMap = collections.defaultdict(set) # keeps track of followees for each user
+        
+        self.postMap = collections.defaultdict(list) # maps a user id to their posts
 
     def postTweet(self, userId: int, tweetId: int) -> None:
-        self.tweetMap[userId].append([self.count, tweetId])
-        self.count -= 1 # decrement count because we want negative values for the max heap
+        self.postMap[userId].append([tweetId, self.time])
+        self.time -= 1
 
     def getNewsFeed(self, userId: int) -> List[int]:
+        posts = []
+
+        self.userMap[userId].add(userId) # user should be able to see their own posts (their own follower)
+
+        for user in self.userMap[userId]:
+            if not self.postMap[user]:
+                continue
+
+            tweetId, time = self.postMap[user][-1] # most recent post
+            posts.append([time, tweetId, user, len(self.postMap[user]) - 1]) # time, tweetId, user, index
+
+        heapq.heapify(posts)
         res = []
-        maxHeap = []
 
-        # user should "follow" themselves (needed for algorithm to also include user's own posts in their feed)
-        self.followMap[userId].add(userId)
-        
-        # initialize maxHeap
-        for user in self.followMap[userId]:
-            # if current user has tweets/posts
-            if self.tweetMap[user]:
-                index = len(self.tweetMap[user]) - 1
-                count, tweetId = self.tweetMap[user][index]
-
-                # add count, tweetId, userId of user that posted the tweet, and next index to max heap
-                maxHeap.append([count, tweetId, user, index - 1])
-
-        # heapify the heap (used to save some time complexity - more efficient than pushing k items to heap)
-        heapq.heapify(maxHeap)
-
-        # build output list
-        while len(res) < 10 and maxHeap:
-            count, tweetId, user, index = heapq.heappop(maxHeap)
-
+        while posts and len(res) < 10:
+            time, tweetId, user, index = heapq.heappop(posts)
             res.append(tweetId)
 
-            if index >= 0:
-                count, tweetId = self.tweetMap[user][index]
-
-                heapq.heappush(maxHeap, [count, tweetId, user, index - 1])
+            if index - 1 >= 0:
+                tweetId, time = self.postMap[user][index - 1]
+                heapq.heappush(posts, [time, tweetId, user, index - 1])
 
         return res
 
     def follow(self, followerId: int, followeeId: int) -> None:
-        self.followMap[followerId].add(followeeId)
+        self.userMap[followerId].add(followeeId)
+        
 
     def unfollow(self, followerId: int, followeeId: int) -> None:
-        if followeeId in self.followMap[followerId]:
-            self.followMap[followerId].remove(followeeId)
+        if followeeId in self.userMap[followerId]:
+            self.userMap[followerId].remove(followeeId)
+        
 
 
 # Your Twitter object will be instantiated and called as such:
