a5d1e7ba71d51d614a929a1e3a0c986fab00500b
Hamza Mostafa <hamza.oruba@gmail.com>
2024-04-16 20:03:27 -0400

    Time: 112 ms (96.89%) | Memory: 31.2 MB (62.92%) - LeetSync

--- 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.py
+++ 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.py
@@ -6,21 +6,15 @@
 #         self.right = right
 class Solution:
     def goodNodes(self, root: TreeNode) -> int:
-
-    
-
         self.count = 0
 
         def dfs(node, maxSeenSoFar):
             if not node:
                 return
 
-            # check if node is good
             if node.val >= maxSeenSoFar:
                 self.count += 1
-
-            # update max value
-            maxSeenSoFar = max(maxSeenSoFar, node.val)
+                maxSeenSoFar = node.val
 
             dfs(node.left, maxSeenSoFar)
             dfs(node.right, maxSeenSoFar)
@@ -28,12 +22,4 @@ class Solution:
         dfs(root, float('-inf'))
 
         return self.count
-
-        #  maxSeenSoFar: 3
-
-        # 3 good
-
-    #  2      5 good
-
-
         
