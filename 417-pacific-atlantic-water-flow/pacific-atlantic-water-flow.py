f7accaa0172cc816d28eaffdaa8f18fb40091f2e
Hamza Mostafa <hamza.oruba@gmail.com>
2024-04-17 15:37:42 -0400

    Time: 202 ms (85.63%) | Memory: 18.1 MB (82.34%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
@@ -1,46 +1,36 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        pacific = set()
+        atlantic = set()
         ROWS, COLS = len(heights), len(heights[0])
-        pac, atl = set(), set()
+        directions = [[1, 0], [0, 1], [-1, 0], [0, -1]]
 
-        # dfs function to explore cells that can travel to ocean
-        def dfs(row, col, oceanSet, prevHeight):
-            # already visited, out of bounds, or invalid (prev height is greater than current height)
-            if (row, col) in oceanSet or row < 0 or row >= ROWS or col < 0 or col >= COLS or heights[row][col] < prevHeight:
+        def dfs(row, col, visitSet, prevHeight):
+            # out of bounds or already visited
+            if row < 0 or row >= ROWS or col < 0 or col >= COLS or (row, col) in visitSet:
                 return
 
-            # mark as visited
-            oceanSet.add((row, col))
+            # we must move to greater or equal heights (going backwards)
+            # so if prevHeight is greater than current height then this is an invalid path
+            if prevHeight > heights[row][col]:
+                return
 
-            curHeight = heights[row][col]
+            visitSet.add((row, col))
 
-            # explore all other neighbouring cells
-            dfs(row + 1, col, oceanSet, curHeight)
-            dfs(row - 1, col, oceanSet, curHeight)
-            dfs(row, col + 1, oceanSet, curHeight)
-            dfs(row, col - 1, oceanSet, curHeight)
+            for x, y in directions:
+                dfs(row + x, col + y, visitSet, heights[row][col])
 
-        # start at the top (pacific) and bottom (atlantic) rows and go as deep as you can with dfs
-        # mark each cell as visited
+        # top row -> pacific
+        # bottom row -> atlantic
+        for col in range(COLS):
+            dfs(0, col, pacific, -1)
+            dfs(ROWS - 1, col, atlantic, -1)
 
-        # if a cell is marked as visited by one of the ocean sets then it means that cell can reach that ocean
+        # leftmost col -> pacific
+        # rightmost col -> atlantic
+        for row in range(ROWS):
+            dfs(row, 0, pacific, -1)
+            dfs(row, COLS - 1, atlantic, -1)
 
-        # we are working backwards (from ocean to cells and not from all possible cells to ocean to save time 
-        # complexity). so previous height must be less than or equal to current height (since we go backwards)
-
-        # dfs on rows (top and bottom):
-        for c in range(COLS):
-            dfs(0, c, pac, -1)
-            dfs(ROWS - 1, c, atl, -1)
-
-        # same thing but for columns (leftmost for pacific and rightmost for atlantic)
-        for r in range(ROWS):
-            dfs(r, 0, pac, -1)
-            dfs(r, COLS - 1, atl, -1)
-
-        # if a cell is in both ocean visited cells then that means that cell can reach both oceans and we
-        # should return it as part of our output list
-
-        # '&' operator is a shortcut - returns all elements in the intersection of both sets (in common between 
-        # both sets)
-        return pac & atl
+        return pacific & atlantic
+        
