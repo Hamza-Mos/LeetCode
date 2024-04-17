7ae647d16cc9de6b7aaf9ed25b10da2ca9c99697
Hamza Mostafa <hamza.oruba@gmail.com>
2024-04-17 15:52:02 -0400

    Time: 112 ms (90.88%) | Memory: 17.9 MB (84.24%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.py
+++ 130-surrounded-regions/surrounded-regions.py
@@ -3,52 +3,37 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
+        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
         ROWS, COLS = len(board), len(board[0])
-        # algorithm overview:
 
-        # **REVERSE THINKING**
-        # instead of approaching this problem by capturing all surrounded regions,
-        # we can be a bit more clever and approach this problem by capturing everything BUT unsurrounded regions!
-        # We will do this by marking all O's that cannot be flipped (either at the border or beside another O
-        # that cannot be flipped).
-
-        # steps:
-        
-        # 1. call dfs and mark all non-flippable O's by converting them into T's
-        # 2. loop over whole board and convert the flippable O's to X's and T's (non-flippable O's) back to O's
-
-        # dfs function to mark non-flippable O's
-        def capture(row, col):
-            # out of bounds or not an "O"
-            if row < 0 or row >= ROWS or col < 0 or col >= COLS or board[row][col] != "O":
+        def dfs(row, col):
+            if row < 0 or row >= ROWS or col < 0 or col >= COLS:
                 return
 
-            # convert to T
+            if board[row][col] == "T" or board[row][col] == "X":
+                return
+
             board[row][col] = "T"
 
-            # visit neighbouring cells
-            capture(row + 1, col)
-            capture(row - 1, col)
-            capture(row, col + 1)
-            capture(row, col - 1)
+            for x, y in directions:
+                dfs(row + x, col + y)
 
-        # step 1: mark all non-flippable O's (starting at the border)
-        # bottom and top rows
-        for c in range(COLS):
-            capture(0, c)
-            capture(ROWS - 1, c)
+        # go over borders to capture the unsurrounded regions
+        for row in range(ROWS):
+            dfs(row, 0)
+            dfs(row, COLS - 1)
 
-        # left and right columns
-        for r in range(ROWS):
-            capture(r, 0)
-            capture(r, COLS - 1)
+        for col in range(COLS):
+            dfs(0, col)
+            dfs(ROWS - 1, col)
 
-        # step 2: loop over whole board and convert the flippable O's to X's and T's (non-flippable O's) back to O's
-        for r in range(ROWS):
-            for c in range(COLS):
-                if board[r][c] == "O":
-                    board[r][c] = "X"
+        for i in range(ROWS):
+            for j in range(COLS):
+                if board[i][j] == "O":
+                    board[i][j] = "X"
 
-                elif board[r][c] == "T":
-                    board[r][c] = "O"
+                elif board[i][j] == "T":
+                    board[i][j] = "O"
+
+        
         
