e23bb743011c4c18e3575b07a14dd0ecd810b430
Hamza Mostafa <hamza.oruba@gmail.com>
2024-04-19 09:47:46 -0400

    Time: 72 ms (60.20%) | Memory: 19.8 MB (89.10%) - LeetSync

--- 252-meeting-rooms/meeting-rooms.py
+++ 252-meeting-rooms/meeting-rooms.py
@@ -1,10 +1,11 @@
 class Solution:
     def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
-        intervals.sort()
+        intervals.sort() # sort by start time
 
         prevEnd = -1
 
         for start, end in intervals:
+            # overlap (current meeting starts before the previous one ended)
             if start < prevEnd:
                 return False
 
