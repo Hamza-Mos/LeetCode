a08cb98d8eadb85b208a187ce9b7c8cf41f18741
Hamza Mostafa <hamza.oruba@gmail.com>
2024-04-19 07:07:36 -0400

    Time: 45 ms (32.83%) | Memory: 16.5 MB (89.45%) - LeetSync

--- 768-partition-labels/partition-labels.py
+++ 768-partition-labels/partition-labels.py
@@ -12,8 +12,11 @@ class Solution:
 
         # partition string
         for index, char in enumerate(s):
+            # endOfCurrentPartition should always point to the last index of any character in the current
+            # partition
             endOfCurrentPartition = max(lastIndexMap[char], endOfCurrentPartition)
 
+            # we have reached the end of the current partition so append length to the result list
             if index == endOfCurrentPartition:
                 res.append(endOfCurrentPartition - startOfCurrentPartition + 1)
                 startOfCurrentPartition = index + 1
