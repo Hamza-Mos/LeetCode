7217206d32fae94408099c95602478d3a699e823
Hamza Mostafa <hamza.oruba@gmail.com>
2024-04-23 20:18:06 -0400

    Time: 213 ms (49.52%) | Memory: 37.7 MB (34.05%) - LeetSync

---
+++ 494-target-sum/target-sum.py
@@ -0,0 +1,24 @@
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        # two states for each number: positive or negative
+        # in our dp cache, to keep track of combinations for each number, we need to save the index and current sum
+
+        dp = {}
+
+        def dfs(index, currSum):
+            if index == len(nums):
+                if currSum == target:
+                    return 1
+
+                else:
+                    return 0
+
+            if (index, currSum) in dp:
+                return dp[(index, currSum)]
+
+            dp[(index, currSum)] = dfs(index + 1, currSum - nums[index]) + dfs(index + 1, currSum + nums[index])
+
+            return dp[(index, currSum)]
+
+        return dfs(0, 0)
+        
