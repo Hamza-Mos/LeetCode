f03c9ae80a3d542221858bd6838f27d2d99255ad
Hamza Mostafa <hamza.oruba@gmail.com>
2024-04-28 16:35:49 -0400

    Time: 1611 ms (44.13%) | Memory: 61.8 MB (5.08%) - LeetSync

--- 2026-merge-triplets-to-form-target-triplet/merge-triplets-to-form-target-triplet.py
+++ 2026-merge-triplets-to-form-target-triplet/merge-triplets-to-form-target-triplet.py
@@ -7,7 +7,7 @@ class Solution:
         goodElements = set()
 
         for triplet in triplets:
-            if any([tripletNum > targetNum for tripletNum, targetNum in zip(triplet, target)]):
+            if triplet[0] > target[0] or triplet[1] > target[1] or triplet[2] > target[2]:
                 continue
 
             for index, num in enumerate(triplet):
