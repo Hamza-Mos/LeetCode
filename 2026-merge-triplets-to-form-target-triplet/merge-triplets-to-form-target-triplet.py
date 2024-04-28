6f122984b88af479d0076781a25cff620c3169d5
Hamza Mostafa <hamza.oruba@gmail.com>
2024-04-28 16:34:48 -0400

    Time: 1738 ms (6.79%) | Memory: 61.9 MB (5.08%) - LeetSync

--- 2026-merge-triplets-to-form-target-triplet/merge-triplets-to-form-target-triplet.py
+++ 2026-merge-triplets-to-form-target-triplet/merge-triplets-to-form-target-triplet.py
@@ -1,21 +1,20 @@
 class Solution:
     def mergeTriplets(self, triplets: List[List[int]], target: List[int]) -> bool:
-        # ignore triplets that have any value that is greater than the greatest value in the target array
+        # get rid of triplets that have any element greater than the greatest element in target
+        # keep track of good elements that match up with elements in target in a set
+        # if size of set is 3 then we have all elements needed to get to target
 
-        # set of all positions in triplets that match positions/values in the target list
-        goodPositions = set()
+        goodElements = set()
 
-        for t in triplets:
-            # ignore triplets that have a value greater than the greatest value in target
-            if t[0] > target[0] or t[1] > target[1] or t[2] > target[2]:
+        for triplet in triplets:
+            if any([tripletNum > targetNum for tripletNum, targetNum in zip(triplet, target)]):
                 continue
 
-            for index, val in enumerate(t):
-                # found a good value
-                if val == target[index]:
-                    # we include index because it is possible that multiple triplets have 
-                    # the same value at the same index so we should count them as 1 good position
-                    goodPositions.add((index, val))
+            for index, num in enumerate(triplet):
+                if num == target[index]:
+                    print([index, num])
+                    goodElements.add((index, num))
 
-        return len(goodPositions) == 3 # found all 3 positions that match target
-        
+        print(goodElements)
+
+        return len(goodElements) == 3
