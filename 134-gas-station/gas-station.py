92d63e8649ae2c4b1ec84b8a6da99fa00df08120
Hamza Mostafa <hamza.oruba@gmail.com>
2024-04-28 16:09:43 -0400

    Time: 914 ms (93.83%) | Memory: 22.1 MB (53.68%) - LeetSync

--- 134-gas-station/gas-station.py
+++ 134-gas-station/gas-station.py
@@ -1,17 +1,18 @@
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
-        # impossible
+        # not possible
         if sum(gas) < sum(cost):
             return -1
 
-        total = 0
-        start = 0 # we want to start at an index where the diff between gas and cost becomes positive
+        diff = 0
+        start = 0
 
         for index in range(len(gas)):
-            total += gas[index] - cost[index] 
+            if diff < 0:
+                diff = 0
+                start = index
 
-            if total < 0:
-                total = 0
-                start = index + 1
+            diff += gas[index] - cost[index]
 
         return start
+        
