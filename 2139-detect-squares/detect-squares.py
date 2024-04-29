a71f3b115ff9c8439af0ca4064a003ce128cd348
Hamza Mostafa <hamza.oruba@gmail.com>
2024-04-29 08:51:14 -0400

    Time: 390 ms (37.09%) | Memory: 18.7 MB (22.92%) - LeetSync

---
+++ 2139-detect-squares/detect-squares.py
@@ -0,0 +1,31 @@
+class DetectSquares:
+
+    def __init__(self):
+        self.points = []
+        self.pointsCount = defaultdict(int)
+        
+
+    def add(self, point: List[int]) -> None:
+        self.points.append(tuple(point))
+        self.pointsCount[tuple(point)] += 1
+        
+
+    def count(self, point: List[int]) -> int:
+        res = 0
+
+        pointX, pointY = point
+
+        for x, y in self.points:
+            # check if the current point is diagonal with input point
+            # then check if the rest of the corners of the square (two other points) make a square
+            if abs(x - pointX) == abs(y - pointY) and x != pointX and y != pointY:
+                res += self.pointsCount[(x, pointY)] * self.pointsCount[(pointX, y)]
+
+        return res
+        
+
+
+# Your DetectSquares object will be instantiated and called as such:
+# obj = DetectSquares()
+# obj.add(point)
+# param_2 = obj.count(point)
