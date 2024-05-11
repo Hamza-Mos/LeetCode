840e558ad2d24558e20b8b48c57364ad6c923052
Hamza Mostafa <hamza.oruba@gmail.com>
2024-05-11 19:33:08 -0400

    Time: 35 ms (66.56%) | Memory: 16.6 MB (65.36%) - LeetSync

--- 190-reverse-bits/reverse-bits.py
+++ 190-reverse-bits/reverse-bits.py
@@ -3,9 +3,16 @@ class Solution:
         res = 0
 
         for i in range(32):
+            # move res 1 bit left
             res = res << 1
+
+            # get rightmost bit for n
             bit = n & 1
+
+            # add bit to end of res
             res = res | bit
+
+            # bitshift n to the right
             n = n >> 1
 
         return res
