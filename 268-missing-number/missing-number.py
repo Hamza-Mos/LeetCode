63d014d9298111a7a15affe2f298814993eec178
Hamza Mostafa <hamza.oruba@gmail.com>
2024-05-12 14:15:52 -0400

    Time: 112 ms (56.47%) | Memory: 17.9 MB (17.14%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -1,5 +1,7 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
+        # use XOR
+        # ex: (0 ^ 0) ^ (1 ^ 1) ^ 2 = (0) ^ (0) ^ 2 = 2
         missingNum = len(nums)
 
         for i in range(len(nums)):
