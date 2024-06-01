c086f9f1edaeb743e012f9b0b2a7af2498133bc1
Hamza Mostafa <hamza.oruba@gmail.com>
2024-06-01 19:39:24 -0400

    Time: 1099 ms (60.69%) | Memory: 33.2 MB (27.05%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.py
+++ 239-sliding-window-maximum/sliding-window-maximum.py
@@ -1,29 +1,28 @@
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        # monotonically decreasing queue
-        q = deque() # will contain (num, index)
+        # decreasing monotonic queue
         res = []
 
-        for i in range(k):
-            while q and q[-1][0] <= nums[i]:
-                q.pop()
-            
-            q.append((nums[i], i))
+        q = deque()
 
-        res.append(q[0][0])
+        for i in range(k):
+            while q and q[-1][1] < nums[i]:
+                q.pop()
+
+            q.append([i, nums[i]])
+
+        res.append(q[0][1])
 
         for i in range(k, len(nums)):
-            # pop elements that are out of the current window
-            while q and q[0][1] <= i - k:
+            while q and q[0][0] <= i - k:
                 q.popleft()
 
-            # keep queue monotonically decreasing
-            while q and q[-1][0] <= nums[i]:
+            while q and q[-1][1] < nums[i]:
                 q.pop()
 
-            q.append((nums[i], i))
+            q.append([i, nums[i]])
 
-            res.append(q[0][0])
+            res.append(q[0][1])
 
         return res
-            
+        
