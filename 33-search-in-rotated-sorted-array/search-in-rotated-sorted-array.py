e771de3e1775479e6297c80063f64f84e9b319a6
Hamza Mostafa <hamza.oruba@gmail.com>
2024-06-02 13:15:05 -0400

    Time: 35 ms (93.74%) | Memory: 16.7 MB (99.10%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -1,31 +1,32 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        left, right = 0, len(nums) - 1
+        low, high = 0, len(nums) - 1
 
-        while left <= right:
-            mid = (left + right) // 2
+        while low <= high:
+            mid = (low + high) // 2
 
-            # found target
             if nums[mid] == target:
                 return mid
 
-            # find current partition
+            # find out which partition we are in
 
-            # left partition
-            if nums[mid] > nums[right]:
-                if nums[left] <= target < nums[mid]:
-                    right = mid - 1
+            # left sorted partition
+            if nums[mid] > nums[high]:
+                # target is to our left
+                if nums[low] <= target < nums[mid]:
+                    high = mid - 1
 
                 else:
-                    left = mid + 1
+                    low = mid + 1
 
-            # right partition
+            # right sorted partition
             else:
-                if nums[mid] < target <= nums[right]:
-                    left = mid + 1
+                # target is to our right
+                if nums[mid] < target <= nums[high]:
+                    low = mid + 1
 
                 else:
-                    right = mid - 1
+                    high = mid - 1
 
         return -1
         
