b1ccabca6528283657607b3e3946dce4aa463961
Hamza Mostafa <hamza.oruba@gmail.com>
2024-06-02 18:41:06 -0400

    Time: 33 ms (89.29%) | Memory: 16.6 MB (86.04%) - LeetSync

--- 90-subsets-ii/subsets-ii.py
+++ 90-subsets-ii/subsets-ii.py
@@ -1,31 +1,25 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
-        # sort the numbers to easily manage duplicates
         nums.sort()
-        
         res = []
         currSet = []
 
-        def backtrack(index):
+        def dfs(index):
             if index == len(nums):
-                res.append(currSet[:])
+                res.append(currSet[::])
                 return
 
-            # add current number
             currSet.append(nums[index])
-            backtrack(index + 1)
+            dfs(index + 1)
 
-            # remove all instances of nums[index] (duplicates)
             currSet.pop()
 
-            # now nums[index] will point to last occurrence of that number
             while index + 1 < len(nums) and nums[index] == nums[index + 1]:
                 index += 1
 
-            # index + 1 will point to next unique number (or end of array)
-            backtrack(index + 1)
+            dfs(index + 1)
 
-        backtrack(0)
+        dfs(0)
 
         return res
         
