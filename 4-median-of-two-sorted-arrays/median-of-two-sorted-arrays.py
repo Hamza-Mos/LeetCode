3c7d99018eaf612b5b81c8dea1d29bebe5ae0230
Hamza Mostafa <hamza.oruba@gmail.com>
2024-06-02 13:53:55 -0400

    Time: 79 ms (66.45%) | Memory: 17.1 MB (10.94%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
@@ -3,37 +3,55 @@ class Solution:
         if len(nums1) > len(nums2):
             return self.findMedianSortedArrays(nums2, nums1)
 
-        totalLen = len(nums1) + len(nums2)
-        half = totalLen // 2
+        M, N = len(nums1), len(nums2)
 
-        low, high = 0, len(nums1) - 1
+        # smaller array should be nums1
 
-        while True:
-            mid = (low + high) // 2
+        # low, high represent the number of elements in nums1 that will be in the left partition
+        low, high = 0, M
 
-            # nums1 pivot elements
-            left1 = nums1[mid] if mid >= 0 else float('-inf')
-            right1 = nums1[mid + 1] if mid + 1 < len(nums1) else float('inf')
+        while low <= high:
+            """
+            I was initially confused about why it is (m + n + 1) // 2 instead of just (m + n) // 2 and I think the 
+            following is the reason.
 
-            nums2Pivot = half - (mid + 1) - 1
+            Given that m+n is the total count of elements:
 
-            # nums2 pivot elements
-            left2 = nums2[nums2Pivot] if nums2Pivot >= 0 else float('-inf')
-            right2 = nums2[nums2Pivot + 1] if nums2Pivot + 1 < len(nums2) else float('inf')
+            For the odd number total count of elements, this tips the scale of the number of elements to be 
+            1 extra in the left side of the partitions, allowing you to know the median is one of the elements 
+            just to the left of the partition on either nums1 or nums2. (As in the code 
+            return max(maxLeftA, maxLeftB)).
 
-            # we found correct partition
-            if left1 <= right2 and left2 <= right1:
-                # even
-                if totalLen % 2 == 0:
-                    return (max(left1, left2) + min(right1, right2)) / 2
+            For the case of even number total count of elements, the +1 does not affect the quotient due to the 
+            truncation of the 0.5 part.
+            """
 
-                # odd
+            partitionA = (low + high) // 2
+            partitionB = (M + N + 1) // 2 - partitionA
+
+            # partitionA and partitionB represent the number of elements in the left parition for both arrays
+            # so that's why we subtract 1 to get the index of the max element in the left partition
+            maxLeftA = float('-inf') if partitionA - 1 < 0 else nums1[partitionA - 1]
+            maxLeftB = float('-inf') if partitionB - 1 < 0 else nums2[partitionB - 1]
+
+            minRightA = float('inf') if partitionA >= M else nums1[partitionA]
+            minRightB = float('inf') if partitionB >= N else nums2[partitionB]
+
+            # correct partitions
+            if maxLeftA <= minRightB and maxLeftB <= minRightA:
+                # odd length
+                if (M + N) % 2 == 1:
+                    return max(maxLeftA, maxLeftB)
+
                 else:
-                    return min(right1, right2)
+                    return (max(maxLeftA, maxLeftB) + min(minRightA, minRightB)) / 2
 
-            elif left1 > right2:
-                high = mid - 1
+            # go left
+            elif maxLeftA > minRightB:
+                high = partitionA - 1
 
+            # go right
             else:
-                low = mid + 1
+                low = partitionA + 1
+
         
