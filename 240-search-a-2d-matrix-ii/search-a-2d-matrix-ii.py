5474d7865cfe5f736e12bba1e759336a3ab4d5c1
Hamza Mostafa <hamza.oruba@gmail.com>
2024-06-05 18:48:12 -0400

    Time: 148 ms (12.39%) | Memory: 22.9 MB (67.25%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.py
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.py
@@ -2,26 +2,18 @@ class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         ROWS, COLS = len(matrix), len(matrix[0])
 
-        row = ROWS - 1
-        col = 0
-
-        # keep traversing right until we hit a number that is greater than our target (move to larger numbers)
-        # then we keep traversing up until we hit a number less than our target (move to smaller numbers)
-        # repeat until we end loop or reach target
+        row, col = ROWS - 1, 0
 
         while row >= 0 and col < COLS:
-            # found target
             if matrix[row][col] == target:
                 return True
 
-            # current number is less than target so we move right to larger numbers
-            elif matrix[row][col] < target:
-                col += 1
-
-            # current number is greater than target so we move up to smaller numbers
-            else:
+            # need to move to a lower number
+            elif matrix[row][col] > target:
                 row -= 1
 
+            # move to higher number
+            else:
+                col += 1
+
         return False
-            
-        
