3b82e6edb6722a37ab96d0c79d3c9487dc51d7ef
Hamza Mostafa <hamza.oruba@gmail.com>
2024-06-09 21:08:08 -0400

    Time: 37 ms (61.61%) | Memory: 16.5 MB (97.68%) - LeetSync

--- 1297-maximum-number-of-balloons/maximum-number-of-balloons.py
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.py
@@ -1,18 +1,11 @@
 class Solution:
     def maxNumberOfBalloons(self, text: str) -> int:
-        inputFreq = defaultdict(int)
+        inputFreq = {"b": 0, "a": 0, "l": 0, "o": 0, "n": 0}
         for char in text:
-            inputFreq[char] += 1
+            if char in inputFreq:
+                inputFreq[char] += 1
 
-        res = 0
-
-        while inputFreq["b"] and inputFreq["a"] and inputFreq["n"] and inputFreq["l"] >= 2 and inputFreq["o"] >= 2:
-            res += 1
-            inputFreq["b"] -= 1
-            inputFreq["a"] -= 1
-            inputFreq["l"] -= 2
-            inputFreq["o"] -= 2
-            inputFreq["n"] -= 1
-
-        return res
+        inputFreq["l"] = inputFreq["l"] // 2
+        inputFreq["o"] = inputFreq["o"] // 2
+        return min(inputFreq.values())
         
