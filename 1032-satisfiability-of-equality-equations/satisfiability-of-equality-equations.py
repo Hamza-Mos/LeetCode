66c77f365cae6d089f90783d9ddb0678f7512dae
Hamza Mostafa <hamza.oruba@gmail.com>
2024-07-12 21:32:59 -0400

    Time: 45 ms (73.57%) | Memory: 16.6 MB (59.51%) - LeetSync

--- 1032-satisfiability-of-equality-equations/satisfiability-of-equality-equations.py
+++ 1032-satisfiability-of-equality-equations/satisfiability-of-equality-equations.py
@@ -2,7 +2,6 @@ class UnionFind:
     def __init__(self):
         self.parent = {}
         self.rank = {}
-        self.notEqualTo = defaultdict(set)
 
     def addNode(self, n):
         if n not in self.parent and n not in self.rank:
@@ -24,28 +23,12 @@ class UnionFind:
 
         return False
 
-    def notEqual(self, n1, n2):
-        self.notEqualTo[n1].add(n2)
-        self.notEqualTo[n2].add(n1)
-
-        p1, p2 = self.find(n1), self.find(n2)
-
-        self.notEqualTo[p1].add(p2)
-        self.notEqualTo[p2].add(p1)
-
-    # return true for successful union
-    # false for already connected
     def union(self, n1, n2):
-        if n1 in self.notEqualTo[n2] or n2 in self.notEqualTo[n1]:
-            return False
 
         p1, p2 = self.find(n1), self.find(n2)
-
-        if p1 in self.notEqualTo[p2] or p2 in self.notEqualTo[p1]:
-            return False
 
         if p1 == p2:
-            return True
+            return
 
         if self.rank[p1] > self.rank[p2]:
             self.rank[p1] += self.rank[p2]
@@ -55,11 +38,8 @@ class UnionFind:
             self.rank[p2] += self.rank[p1]
             self.parent[p1] = p2
 
-        return True
-
 class Solution:
     def equationsPossible(self, equations: List[str]) -> bool:
-        # union find?
 
         uf = UnionFind()
 
