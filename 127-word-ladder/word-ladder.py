1c7912def76d5f8b06f5870c579bb05013849b0a
Hamza Mostafa <hamza.oruba@gmail.com>
2024-07-23 22:35:21 -0400

    Time: 129 ms (69.72%) | Memory: 24.3 MB (11.05%) - LeetSync

--- 127-word-ladder/word-ladder.py
+++ 127-word-ladder/word-ladder.py
@@ -1,47 +1,32 @@
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        # maps a pattern to list of words that match that pattern ('*' is a wildcard)
-        adjList = collections.defaultdict(list)
+        # adjacency list -> map wildcard words to list of words
+        adjList = defaultdict(list)
+        for word in wordList:
+            for i in range(len(word)):
+                adjList[word[:i] + "*" + word[i + 1:]].append(word)
+        
+        q = deque([[beginWord, 1]])
+        visit = set()
 
-        # generate and populate the adjacency list
-        for w in wordList:
-            for j in range(len(w)):
-                currWord = w[:j] + "*" + w[j + 1:]
-                adjList[currWord].append(w)
+        while q:
+            word, numRounds = q.popleft()
 
-        # start with the beginWord
-        queue = deque([beginWord])
-        visited = {beginWord} # set
+            if word in visit:
+                continue
+            visit.add(word)
 
-        res = 1 # length of transformation sequence
+            for i in range(len(word)):
+                wildcard = word[:i] + "*" + word[i + 1:]
 
-        # bfs
-        while queue:
-            currLevelLen = len(queue)
-            
-            # loop over all words in the current level
-            for i in range(currLevelLen):
-                currWord = queue.popleft()
-
-                # if we reached end word then return result
-                if currWord == endWord:
-                    return res
-
-                # otherwise generate all possible patterns of current word
-                for j in range(len(currWord)):
-                    pattern = currWord[:j] + "*" + currWord[j + 1:]
-
-                    # loop over all words matching the current pattern
-                    for word in adjList[pattern]:
-                        # if word has not been visited yet then add it to queue and mark it as visited
-                        if word not in visited:
-                            queue.append(word)
-                            visited.add(word)
-
-            # increment length of transformation sequence
-            res += 1
+                for nextWord in adjList[wildcard]:
+                    if nextWord == endWord:
+                        print(word)
+                        return numRounds + 1
+                    if nextWord not in visit:
+                        q.append([nextWord, numRounds + 1])
 
         return 0
 
-            
+
         
