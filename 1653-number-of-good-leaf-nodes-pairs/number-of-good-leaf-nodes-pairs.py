e4cf34da4f84d59d1e8f7e809d715c5e4c297c89
Hamza Mostafa <hamza.oruba@gmail.com>
2024-07-27 19:12:09 -0400

    Time: 74 ms (92.50%) | Memory: 16.9 MB (68.36%) - LeetSync

---
+++ 1653-number-of-good-leaf-nodes-pairs/number-of-good-leaf-nodes-pairs.py
@@ -0,0 +1,51 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def countPairs(self, root: TreeNode, distance: int) -> int:
+        self.res = 0
+        # array[i] = number of nodes at a distance[i]
+        def dfs(node):
+            # null
+            if not node:
+                return [0] * (distance + 1)
+
+            # leaf node
+            if not node.left and not node.right:
+                result = [0] * (distance + 1)
+                result[0] = 1 # leaf node is at a distance 0 from itself
+                return result
+
+            leftSubTree = dfs(node.left)
+            rightSubTree = dfs(node.right)
+
+            currNode = [0] * (distance + 1)
+
+            # distance shifts by 1 as you go up the tree to the parent
+            for i in range(distance):
+                currNode[i + 1] = leftSubTree[i] + rightSubTree[i]
+
+            prefixSum = 0 # number of good leaf nodes found from this point
+            index1 = distance - 2 # max distance from left subtree
+
+            # prefixSum = 0
+            # for index1 in range(distance - 1, -1, -1):
+            #     prefixSum += leftSubTree[index1]
+            #     if distance - 2 - index1 >= 0:
+            #         self.res += prefixSum * rightSubTree[distance - 2 - index1]
+
+            prefixSum = 0
+            index1 = 0
+            for index2 in range(distance - 2, -1, -1):
+                prefixSum += leftSubTree[index1]
+                self.res += prefixSum * rightSubTree[index2]
+                index1 += 1
+
+            return currNode
+
+        dfs(root)
+        return self.res
+
