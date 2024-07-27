9724290d4ae42fbb617280e15e49d00db04cf1c7
Hamza Mostafa <hamza.oruba@gmail.com>
2024-07-27 17:18:06 -0400

    Time: 42 ms (16.60%) | Memory: 16.5 MB (62.90%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,15 +1,21 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        robCurrHouseAndOne = 0
-        doNotRobCurrHouse = nums[-1]
+        # dp[i] = dp[i + 1] or dp[i + 2] + nums[i]
+        if len(nums) <= 2:
+            return max(nums)
 
-        # need a variable for dp[i] (temp), dp[i + 1] (doNotRobCurrHouse), and dp[i + 2] (robCurrHouseAndOne)
-        # dp[i] = max(nums[i] + dp[i + 2], dp[i + 1])
+        robCurrHouse = nums[-1] # dp[i + 2] + nums[i]
+        skipCurrHouse = max(nums[-2], nums[-1]) # dp[i + 1]
 
-        for i in reversed(range(len(nums) - 1)):
-            temp = max(robCurrHouseAndOne + nums[i], doNotRobCurrHouse)
-            robCurrHouseAndOne = doNotRobCurrHouse
-            doNotRobCurrHouse = temp
+        for i in range(len(nums) - 3, -1, -1):
+            temp = max(robCurrHouse + nums[i], skipCurrHouse)
+            robCurrHouse = skipCurrHouse
+            skipCurrHouse = temp
 
-        return doNotRobCurrHouse
+        # [2, 1, 1, 2]
+        # temp: 3
+        # robCurrHouse: 1
+        # skipCurrHouse: 3
+
+        return skipCurrHouse
         
