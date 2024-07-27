fc8a71257dd7537bf4ffadf4be902dd58fa3a4df
Hamza Mostafa <hamza.oruba@gmail.com>
2024-07-27 14:50:34 -0400

    Time: 2460 ms (27.16%) | Memory: 186.9 MB (10.57%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.py
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.py
@@ -1,49 +1,41 @@
+from heapq import heappop, heappush
+
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
-        res = 0
-        adjList = collections.defaultdict(list)
-        N = len(points)
+        # find distance from each point to every other point
+        # prim's algo
+        
+        adjList = defaultdict(list)
 
-        for i in range(N):
-            x1, y1 = points[i]
-            for j in range(i + 1, N):
+        for i in range(len(points)):
+            for j in range(i + 1, len(points)):
+                x1, y1 = points[i]
                 x2, y2 = points[j]
+                dist = abs(x1 - x2) + abs(y1 - y2)
 
-                dist = abs(x2 - x1) + abs(y2 - y1)
-
-                # append (destination point, distance to that point)
                 adjList[i].append([j, dist])
                 adjList[j].append([i, dist])
 
-        # min heap will contain distances to neighbouring points ([dist, src point, dest point])
-        # initialize min heap to start at points[0]
-        minHeap = [[0, 0, 0]]
 
-        # set to keep track of all visited nodes
+        # start at point 0
         visited = set()
+        minHeap = [[0, 0, 0]] # dist, src, dest
+        res = 0
 
-        while len(visited) < N:
-            dist, src, dest = heapq.heappop(minHeap)
+        while len(visited) < len(points):
+            dist, src, dest = heappop(minHeap)
 
-            # if dest point has been visited then move onto next element in heap
             if dest in visited:
                 continue
 
-            # mark the dest point as visited
             visited.add(dest)
 
             res += dist
 
-            # loop over all neighbouring points from destination point
             for nextPoint, nextDist in adjList[dest]:
-                # if point is already visited then continue to next iteration of loop
                 if nextPoint in visited:
                     continue
 
-                # dest now becomes new src point, and we append the distance to next along with next point to heap
-                heapq.heappush(minHeap, [nextDist, dest, nextPoint])
-
+                heappush(minHeap, [nextDist, dest, nextPoint])
 
         return res
-
-        
