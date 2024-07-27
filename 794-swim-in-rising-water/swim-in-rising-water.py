8b3341396cfce0220823e3a536087d40551148b1
Hamza Mostafa <hamza.oruba@gmail.com>
2024-07-27 15:23:26 -0400

    Time: 102 ms (31.84%) | Memory: 17.3 MB (31.98%) - LeetSync

--- 794-swim-in-rising-water/swim-in-rising-water.py
+++ 794-swim-in-rising-water/swim-in-rising-water.py
@@ -1,28 +1,25 @@
 class Solution:
     def swimInWater(self, grid: List[List[int]]) -> int:
-        ROWS = COLS = len(grid)
+        ROWS, COLS = len(grid), len(grid[0])
         directions = [[1, 0], [0, 1], [-1, 0], [0, -1]]
-
-        # [time, row, col]
-        minHeap = [[grid[0][0], 0, 0]]
+        minHeap = [[grid[0][0], 0, 0]] # time, row, col
         visited = set()
 
         while minHeap:
-            currTime, row, col = heapq.heappop(minHeap)
-
-            # have reached target cell
-            if (row, col) == (ROWS - 1, COLS - 1):
-                return currTime
+            time, row, col = heapq.heappop(minHeap)
 
             if (row, col) in visited:
                 continue
+
+            if (row, col) == (ROWS - 1, COLS - 1):
+                return time
 
             visited.add((row, col))
 
             for x, y in directions:
                 if 0 <= row + x < ROWS and 0 <= col + y < COLS and (row + x, col + y) not in visited:
-                    time = max(grid[row + x][col + y], currTime)
-                    heapq.heappush(minHeap, [time, row + x, col + y])
+                    newTime = max(time, grid[row + x][col + y])
+                    heapq.heappush(minHeap, [newTime, row + x, col + y])
 
-        return -1
+            
         
