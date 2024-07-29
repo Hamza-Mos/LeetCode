77800c85e17e80fcd085b43e71bf887d74dfdfb0
Hamza Mostafa <hamza.oruba@gmail.com>
2024-07-28 22:23:30 -0400

    Time: 741 ms (94.78%) | Memory: 57.1 MB (76.08%) - LeetSync

--- 1803-average-waiting-time/average-waiting-time.py
+++ 1803-average-waiting-time/average-waiting-time.py
@@ -4,14 +4,14 @@ class Solution:
         res = 0
 
         for time, wait in customers:
-            if time > currTime:
+            if time > currTime: # customer order time is later than the time that the prev order finished
                 currTime = time
 
-            else:
-                res += currTime - time
+            else: # time of previous order and current order overlaps
+                res += currTime - time # add the additional wait time
 
             res += wait
-            currTime += wait
+            currTime += wait # update currTime to the time that the order time will be finished
 
         return res / len(customers)
         
