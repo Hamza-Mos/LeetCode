626e5f10461a4d7df9fc2e3c28ff55c86cf737f1
Hamza Mostafa <hamza.oruba@gmail.com>
2024-07-30 19:50:26 -0400

    Time: 158 ms (46.85%) | Memory: 23.6 MB (24.34%) - LeetSync

--- 1397-search-suggestions-system/search-suggestions-system.py
+++ 1397-search-suggestions-system/search-suggestions-system.py
@@ -1,40 +1,49 @@
 class TrieNode:
     def __init__(self):
+        self.words = []
         self.children = {}
-        self.wordList = []
-
-class Trie:
-    def __init__(self, words):
-        self.root = TrieNode()
-        for word in words:
-            self.insertWord(word)
-
-    def insertWord(self, word):
-        curr = self.root
-        curr.wordList.append(word)
-
-        for char in word:
-            if char not in curr.children:
-                curr.children[char] = TrieNode()
-
-            curr = curr.children[char]
-            curr.wordList.append(word)
 
 class Solution:
     def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
-        trie = Trie(products)
-        currNode = trie.root
+        root = TrieNode()
         res = []
 
-        for char in searchWord:
-            if char not in currNode.children:
-                break
-            
-            currNode = currNode.children[char]
-            res.append(sorted(currNode.wordList)[:3])
+        # functions for trie
+        def insertWord(word):
+            node = root
+            node.words.append(word)
 
-        # Fill the remaining slots with empty lists if we broke early
-        while len(res) < len(searchWord):
+            for char in word:
+                if char not in node.children:
+                    node.children[char] = TrieNode()
+
+                node = node.children[char]
+                node.words.append(word)
+
+        for product in products:
+            insertWord(product)
+
+        index = 0
+
+        while index < len(searchWord):
+            char = searchWord[index]
+
+            if char in root.children:
+                root = root.children[char]
+                print(char, root.words, root.children)
+                res.append(sorted(root.words)[:3])
+
+            else:
+                break
+
+            index += 1
+
+        while index < len(searchWord):
             res.append([])
+            index += 1
 
         return res
+
+                
+            
+        
