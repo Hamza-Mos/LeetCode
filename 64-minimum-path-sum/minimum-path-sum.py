e311dfe11306b472edc13bd2e154cdb7e6bcd41f
Hamza Mostafa <hamza.oruba@gmail.com>
2024-08-15 20:26:39 -0400

    Time: 87 ms (45.29%) | Memory: 18.3 MB (75.94%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.py
+++ 64-minimum-path-sum/minimum-path-sum.py
@@ -6,7 +6,11 @@ class Solution:
 
         for row in range(ROWS - 1, -1, -1):
             for col in range(COLS - 1, -1, -1):
-                rows[row][col] = min(rows[row][col], rows[row][col + 1] + grid[row][col], rows[row + 1][col] + grid[row][col])
+                rows[row][col] = min(
+                    rows[row][col], 
+                    rows[row][col + 1] + grid[row][col], 
+                    rows[row + 1][col] + grid[row][col]
+                )
 
         return rows[0][0]
 
