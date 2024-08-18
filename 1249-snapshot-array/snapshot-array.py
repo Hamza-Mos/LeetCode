0a1e4076caeb0db5499fe9833b14cdb12c7e7aba
Hamza Mostafa <hamza.oruba@gmail.com>
2024-08-18 10:41:30 -0400

    Time: 667 ms (28.22%) | Memory: 45.8 MB (53.72%) - LeetSync

--- 1249-snapshot-array/snapshot-array.py
+++ 1249-snapshot-array/snapshot-array.py
@@ -1,24 +1,55 @@
 class SnapshotArray:
 
     def __init__(self, length: int):
-        self.array = [[[0, 0]] for i in range(length)]
+        # 2D array
+        # basically an array of given length
+        # each element contains an array of tuples (snapId, value)
+
+        self.vals = [ [[0, 0]] for row in range(length) ]
         self.snapId = 0
+
         
 
     def set(self, index: int, val: int) -> None:
-        self.array[index].append([self.snapId, val])
+        latestSnapId = self.vals[index][-1][0]
+
+        if latestSnapId == self.snapId:
+            # sets latest snap value to val
+            self.vals[index][-1][1] = val
+
+        else:
+            # add tuple because latest value has outdated snap id
+            self.vals[index].append([self.snapId, val])
         
 
     def snap(self) -> int:
         self.snapId += 1
+
         return self.snapId - 1
         
 
-    # we want to return the latest value for the current index before snap_id + 1
     def get(self, index: int, snap_id: int) -> int:
-        # this will return the rightmost insertion index in the given version snap_index
-        snapIndex = bisect.bisect_right(self.array[index], [snap_id, float('inf')])
-        return self.array[index][snapIndex - 1][1]
+        # look for value with biggest snapId that is <= passed snap_id
+        # binary search
+
+        vals = self.vals[index]
+
+        low, high = 0, len(vals) - 1
+        output = -1
+
+        while low <= high:
+            mid = (low + high) // 2
+
+            snapId, val = vals[mid]
+
+            if snapId <= snap_id:
+                output = val
+                low = mid + 1
+
+            else:
+                high = mid - 1
+
+        return output
         
 
 
