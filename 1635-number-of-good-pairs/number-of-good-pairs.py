18775f0bdc8bcec5a7c7d6bbcbb9d76bba346431
Hamza Mostafa <hamza.oruba@gmail.com>
2024-08-18 16:09:08 -0400

    Time: 26 ms (97.87%) | Memory: 16.5 MB (64.00%) - LeetSync

--- 1635-number-of-good-pairs/number-of-good-pairs.py
+++ 1635-number-of-good-pairs/number-of-good-pairs.py
@@ -1,5 +1,16 @@
 class Solution:
     def numIdenticalPairs(self, nums: List[int]) -> int:
+        """
+        approach:
+
+            - use hashmap to count frequency of numbers
+            - iterate through each number in the array
+                - add the count of the number in the hashmap to the result
+                - increment the frequency of the number by 1
+
+            - return result
+
+        """
         numCount = defaultdict(int)
         res = 0
 
