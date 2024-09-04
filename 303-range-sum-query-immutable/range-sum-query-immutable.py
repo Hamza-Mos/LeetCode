cbed3658bd1b55fe68814e3edd8355995f025e6f
Hamza Mostafa <hamza.oruba@gmail.com>
2024-09-04 11:00:29 -0700

    Time: 67 ms (76.83%) | Memory: 20.2 MB (52.80%) - LeetSync

--- 303-range-sum-query-immutable/range-sum-query-immutable.py
+++ 303-range-sum-query-immutable/range-sum-query-immutable.py
@@ -1,15 +1,16 @@
 class NumArray:
 
+    # use prefix sum!!
     def __init__(self, nums: List[int]):
-        self.nums = nums
+        self.sums = [0] * (len(nums) + 1)
+
+        for i in range(len(nums)):
+            self.sums[i + 1] = self.sums[i] + nums[i]
         
 
     def sumRange(self, left: int, right: int) -> int:
-        res = 0
-        for i in range(left, right + 1):
-            res += self.nums[i]
-
-        return res
+        # basically returns sum of nums up to and including nums[right] - sum of nums up to and including nums[left]
+        return self.sums[right + 1] - self.sums[left]
 
         
 
