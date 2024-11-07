bb55161abddc9147074a58e60cc58cb6cfa5c56f
Hamza Mostafa <hamza.oruba@gmail.com>
2024-11-07 09:44:51 -0800

    Time: 199 ms (44.69%) | Memory: 21.4 MB (21.76%) - LeetSync

---
+++ 170-two-sum-iii-data-structure-design/two-sum-iii-data-structure-design.py
@@ -0,0 +1,26 @@
+class TwoSum:
+
+    def __init__(self):
+        self.count = {}
+        
+
+    def add(self, number: int) -> None:
+        self.count[number] = self.count.get(number, 0) + 1
+        
+
+    def find(self, value: int) -> bool:
+        for num in self.count:
+            if num == value - num:
+                if self.count[num] > 1:
+                    return True
+            elif value - num in self.count:
+                return True
+
+        return False
+        
+
+
+# Your TwoSum object will be instantiated and called as such:
+# obj = TwoSum()
+# obj.add(number)
+# param_2 = obj.find(value)
